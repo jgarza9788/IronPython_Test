84fbaf05e909be7391430a99eb2c8241e1cb1a93
jgarza9788 <jgarza9788@gmail.com>
2020-08-16 11:07:28 -0700

    with readme picture

--- IronPython_Test.py
+++ IronPython_Test.py
@@ -4,8 +4,10 @@ from System.Windows import Application, Window
 
 class MyWindow(Window):
     def __init__(self):
-        wpf.LoadComponent(self, 'IronPython_Test.xaml')
-    
+        self.ui = wpf.LoadComponent(self, 'IronPython_Test.xaml')
+   
+    def Button_Click(self,sender,e):
+        print('hello')
 
 if __name__ == '__main__':
     Application().Run(MyWindow())
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # IronPython_Test
- 
+![image](./readme_resources/2020-08-16_11-05-16.png)
